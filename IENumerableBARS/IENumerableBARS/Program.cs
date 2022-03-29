4f0810d3aa22bce046aea1b8125b0218f985bbf2
Shamil331 <55633377+Shamil331@users.noreply.github.com>
2022-03-29 17:48:20 +0300

    Add files via upload

---
+++ IENumerableBARS/IENumerableBARS/Program.cs
@@ -0,0 +1,26 @@
+﻿using IENumerableBARS;
+
+List<Entity> entity = new List<Entity>();
+string result = "";
+
+entity.Add(new Entity() { Id = 1, ParentId = 0, Name = "Root entity" });
+entity.Add(new Entity() { Id = 2, ParentId = 1, Name = "Child of 1 entity" });
+entity.Add(new Entity() { Id = 3, ParentId = 1, Name = "Child of 1 entity" });
+entity.Add(new Entity() { Id = 4, ParentId = 2, Name = "Child of 2 entity" });
+entity.Add(new Entity() { Id = 5, ParentId = 4, Name = "Child of 4 entity" });
+
+static Dictionary<int, List<Entity>> GetEntites(List<Entity> entities)
+{
+    return entities.GroupBy(x => x.ParentId).ToDictionary(x => x.Key, a => a.ToList());
+}
+
+var entities=GetEntites(entity);
+
+foreach (var i in entities)
+{
+    result += $"\nKey = {i.Key},";
+    result += " Value = List {";
+    i.Value.ForEach(x => result += $" Entity{{Id={x.Id}}}");
+    result += "}";
+}
+Console.WriteLine(result);
